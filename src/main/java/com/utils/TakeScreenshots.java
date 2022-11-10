97814625e02bcc8195f1ff7ca946d90d910a72d8
droanhqQA <110013916+droanhqQA@users.noreply.github.com>
2022-11-10 16:26:11 +0530

    Update TakeScreenshots.java

--- src/main/java/com/utils/TakeScreenshots.java
+++ src/main/java/com/utils/TakeScreenshots.java
@@ -9,14 +9,14 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class TakeScreenshots {
-	static String FILEPATH = ".\\screenshot";
+	static String FILEPATH = "/var/lib/jenkins/screenshot";
 	public void takeScreenShot(String testName,String className, WebDriver driver) {
 		// TODO Auto-generated method stub
 		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		System.out.println("Test Case Name: "+testName);
 		//The below method will save the screen shot in path mentioned with test method name 
            try {
-				FileUtils.copyFile(scrFile, new File(FILEPATH+"\\"+className+"\\"+testName+".png"));
+				FileUtils.copyFile(scrFile, new File(FILEPATH+"/"+className+"/"+testName+".png"));
 				System.out.println("***Placed screen shot in "+FILEPATH+" ***");
 			} catch (IOException e) {
 				e.printStackTrace();
